d7448ef1c1003d3d9a1cb7b39136604a82b8e6a8
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2015-04-26 21:26:23 -0700

    initial commit

---
+++ lib/Acme/CPAN/Ratings.pm
@@ -0,0 +1,100 @@
+package Acme::CPAN::Ratings;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+use Config;
+
+__PACKAGE__->review;
+
+sub loaded_from_bin {
+    $_[1] =~ /^$Config{sitebinexp}\//;
+}
+
+sub review {
+    my $class = shift;
+
+    if ($class->loaded_from_bin($0)) {
+        require File::Basename;
+        my $bin = File::Basename::basename($0);
+        my $dist = $class->find_dist($0);
+        print <<EOF;
+#####################################################################
+# Rate $dist
+#####################################################################
+
+If you enjoy using $bin from $dist, would you mind taking a moment to
+rate it? It won't take more than a minute. Thanks for your support!
+
+Rate it now?    [Y]
+Remind Me Later [r]
+No, Thanks      [n]
+
+EOF
+        chomp(my $input = <STDIN>);
+        if (lc $input eq 'y' or $input eq '') {
+            system 'open', "http://cpanratings.perl.org/dist/$dist";
+        }
+    }
+}
+
+sub lines_of {
+    open my $fh, "<", $_[0];
+    map { chomp; $_ } <$fh>;
+}
+
+sub find_dist {
+    my($class, $bin) = @_;
+
+    require File::Find;
+
+    my %pack_rev;
+    File::Find::find({
+        no_chdir => 1,
+        wanted => sub {
+            return unless m!/\.packlist$!;
+            $pack_rev{$_} = $File::Find::name for lines_of $File::Find::name;
+        },
+    }, @INC);
+
+    if ($pack_rev{$bin}) {
+        (my $dist = $pack_rev{$bin}) =~ s!.*/auto/(.*?)/\.packlist$!$1!;
+        $dist =~ s!/!-!g;
+        $dist;
+    }
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::CPAN::Ratings - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Acme::CPAN::Ratings;
+
+=head1 DESCRIPTION
+
+Acme::CPAN::Ratings is
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Tatsuhiko Miyagawa
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,8 @@
+use strict;
+use Test::More;
+use Acme::CPAN::Ratings;
+
+# replace with the actual test
+ok 1;
+
+done_testing;
